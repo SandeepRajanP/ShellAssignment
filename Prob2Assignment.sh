a9c1774793a890e386966dd78bc6bcbd4e797dd9
SandeepRajanP <sandeeprajan.p>
2018-06-09 00:18:12 +0530

    Problem 2

---
+++ Prob2Assignment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Creating 2 instances
+aws ec2 run-instances --image-id ami-14c5486b --instance-type t2.micro --key-name shellscripting --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=SandeepUploadS3Bucket},{Key=username,Value=SandeepRajanP},{Key=emailId,Value=sandeeprajan.p@quantiphi.com},{Key=Project,Value=PE_Training}]' --iam-instance-profile Arn=arn:aws:iam::488599217855:instance-profile/PE_trainee_Admin_role --user-data  '#!/bin/bash
+echo "This file will be uploaded to ec2instance">upload.txt
+aws s3 cp upload.txt s3://buckettraining007/uploaded.txt'
+
+aws s3 ls s3://buckettraining007/uploaded.txt
+while [ $? -ne 0 ]
+do
+    aws s3 ls s3://buckettraining007/uploaded.txt
+    if [[ $? -eq 0 ]]
+    then
+        break
+    fi
+done
+
+aws ec2 run-instances --image-id ami-14c5486b --instance-type t2.micro --key-name shellscripting --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=SandeepDownloadS3Bucket},{Key=username,Value=SandeepRajanP},{Key=emailId,Value=sandeeprajan.p@quantiphi.com},{Key=Project,Value=PE_Training}]' --iam-instance-profile Arn=arn:aws:iam::488599217855:instance-profile/PE_trainee_Admin_role --user-data '#!/bin/bash
+aws s3 cp s3://buckettraining007/uploaded.txt download.txt'
+
